36145444dedb30478145585c7004b9b64b9628ab
PinkNoteX <omarfarghaly8@gmail.com>
2024-01-23 18:09:00 -0800

    puppet

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,19 @@
+#using puppet to setup nginx
+
+exec { 'nginx':
+        command => '/usr/bin/apt-get update && /usr/bin/apt-get -y install nginx',
+}
+
+file {'/var/www/html/index.nginx-debian.html':
+	content => 'Hello World!'
+}
+
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
+}
+
+exec { 'nginx':
+        command => 'service nginx start',
+	provider => 'shell',
+}
