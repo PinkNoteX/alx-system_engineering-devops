5c122d4aecbbc36e3fdd077ddf18765fc1fd27db
PinkNoteX <omarfarghaly8@gmail.com>
2024-02-19 17:03:09 -0800

    Task 3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
-""" export data to json format """
+""" export all users' data to json format """
 import json
 import requests
-import sys
 
 
 if __name__ == '__main__':
-    NUMBER_OF_DONE_TASKS = 0
-    TASK_TITLE = []
-    u_id = sys.argv[1]
-    name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                        .format(u_id)).json()
-    req = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                       .format(u_id)).json()
-    j_dict = {}
-    j_list = []
-    for i in req:
-        j_dict['task'] = i.get('title')
-        j_dict['completed'] = i.get('completed')
-        j_dict['username'] = i.get(name.get('username'))
-        j_list.append(j_dict)
+    return_j = {}
+    save_j = {}
+    users = requests.get('https://jsonplaceholder.typicode.com/users').json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    for i in users:
+        u_id = i.get('id')
+        return_j[u_id] = []
+        save_j[u_id] = i.get('username')
+    for i in todos:
         j_dict = {}
-    j_dict_r = {}
-    j_dict_r[u_id] = j_list
-    with open(u_id + '.json', 'w') as f:
-        json.dump(j_dict_r, f)
+        u_id = i.get('userId')
+        j_dict['task'] = i.get('title')
+        j_dict['username'] = save_j.get(u_id)
+        j_dict['completed'] = i.get('completed')
+        return_j.setdefault(u_id, []).append(j_dict)
+
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(return_j, f)
