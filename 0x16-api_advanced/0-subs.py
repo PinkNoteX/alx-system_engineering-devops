44dd2d3868c8a7609ffb83733c6a6948b72f755e
PinkNoteX <omarfarghaly8@gmail.com>
2024-03-05 17:41:55 -0800

    Task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-""" nuumber of subs in a subreddit """
+""" return number of subs in subreddit """
 import requests
 
 
 def number_of_subscribers(subreddit):
     """ return number of subs in subreddit """
-    h = {'User-Agent': 'reddit_user'}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    rq = requests.get(url, headers=h, allow_redirects=False)
+    headers = {"User-Agent": "alx_0x16_api_advanced"}
+    rq = requests.get(url, headers=headers, allow_redirects=False)
     if rq.status_code == 200:
-        return (rq.json().get("data").get("subscribers"))
-    return 0
+        data = rq.json()
+        subs = data['data']['subscribers']
+        return subs
+    else:
+        return 0
